f2bec4a8fdb1b822168259149354c10eb6e1f27a
Rob Harrington <oeoeaio@gmail.com>
2015-01-17 14:34:43 +1100

    Include CalculatedAjustments module in EnterpriseFees, remove deprecated call to calculated_adjustments

--- app/models/enterprise_fee.rb
+++ app/models/enterprise_fee.rb
@@ -1,12 +1,11 @@
 class EnterpriseFee < ActiveRecord::Base
+  include Spree::Core::CalculatedAdjustments
   belongs_to :enterprise
   has_and_belongs_to_many :order_cycles, join_table: 'coordinator_fees'
   has_many :exchange_fees, dependent: :destroy
   has_many :exchanges, through: :exchange_fees
 
   before_destroy { order_cycles.clear }
-
-  calculated_adjustments
 
   attr_accessible :enterprise_id, :fee_type, :name, :calculator_type
 
