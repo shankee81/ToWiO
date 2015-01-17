d4830c0e1c1c215043cd87655b8fb0d03419cc68
Rob Harrington <oeoeaio@gmail.com>
2015-01-17 17:02:36 +1100

    Update argument format for Spree::Core::ControllerHelpers::Order#current_order

--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Core::ControllerHelpers::Order.class_eval do
   def current_order_with_scoped_variants(create_order_if_necessary = false)
-    order = current_order_without_scoped_variants(create_order_if_necessary)
+    order = current_order_without_scoped_variants({create_order_if_necessary: create_order_if_necessary})
 
     if order
       order.line_items.each do |li|
