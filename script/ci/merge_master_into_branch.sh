f82f8ca30ec72e9c102afc5a0e39091f2eac6f80
Paul Mackay <pauljmackay@gmail.com>
2015-08-01 20:41:24 +0100

    test commit

--- script/ci/merge_master_into_branch.sh
+++ script/ci/merge_master_into_branch.sh
@@ -7,7 +7,7 @@ echo "--- Checking if master has already been merged into $BUILDKITE_BRANCH"
 set_ofn_commit $BUILDKITE_COMMIT
 succeed_if_master_merged
 
-echo "--- Merging master into this branch ($BUILDKITE_BRANCH) "
+echo "--- Merging master into this branch ($BUILDKITE_BRANCH)"
 git checkout $BUILDKITE_BRANCH
 git merge origin/$BUILDKITE_BRANCH
 git merge origin/master -m "Auto-merge from CI [skip ci]"
