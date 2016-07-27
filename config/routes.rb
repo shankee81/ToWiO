a1a02cbea9cc8cbadd93c24b087d01b64ee70571
Rob Harrington <oeoeaio@gmail.com>
2016-07-27 15:39:29 +1000

    Enterprise User can create and update schedules via OC index

--- config/routes.rb
+++ config/routes.rb
@@ -152,7 +152,7 @@ Openfoodnetwork::Application.routes.draw do
 
     resource :invoice_settings, only: [:edit, :update]
 
-    resources :schedules, only: [:index, :create], format: :json
+    resources :schedules, only: [:index, :create, :update], format: :json
   end
 
   namespace :api do
