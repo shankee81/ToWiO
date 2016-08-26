caa5081a76bd7ea7158e3e67182675172e9d0d39
Rob Harrington <oeoeaio@gmail.com>
2016-08-26 10:29:00 +1000

    SO: Adding basic interface for Standing Order creation

--- config/routes.rb
+++ config/routes.rb
@@ -153,6 +153,8 @@ Openfoodnetwork::Application.routes.draw do
     resource :invoice_settings, only: [:edit, :update]
 
     resources :schedules, only: [:index, :create, :update, :destroy], format: :json
+
+    resources :standing_orders, only: [:new, :create]
   end
 
   namespace :api do
