2cd2976b294e5965ceb9636c4f17590c90aa1c03
Rob Harrington <oeoeaio@gmail.com>
2016-09-07 21:43:57 +1000

    SO: Adding variants to a new standing order, price estimate provided

--- config/routes.rb
+++ config/routes.rb
@@ -148,7 +148,9 @@ Openfoodnetwork::Application.routes.draw do
 
     resources :schedules, only: [:index, :create, :update, :destroy], format: :json
 
-    resources :standing_orders, only: [:new, :create]
+    resources :standing_orders, only: [:new, :create] do
+      get :indicative_variant, on: :collection, format: :json
+    end
   end
 
   namespace :api do
@@ -252,6 +254,10 @@ Spree::Core::Engine.routes.prepend do
       get :invoice, on: :member
       get :print, on: :member
       get :managed, on: :collection
+    end
+
+    resources :variants, only: [], format: :json do
+      get :price_estimate, on: :member
     end
 
     resources :line_items, only: [:index], format: :json
