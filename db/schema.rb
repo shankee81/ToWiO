fe0b01058b9e93dad0c9458710a12badd5ad45a0
Rob Harrington <oeoeaio@gmail.com>
2016-12-07 15:14:07 +1100

    Renaming cancelled_at column to canceled_at, for consistency

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20161215230219) do
 
   create_table "account_invoices", :force => true do |t|
@@ -1066,7 +1065,7 @@ ActiveRecord::Schema.define(:version => 20161215230219) do
   create_table "standing_order_orders", :force => true do |t|
     t.integer  "standing_order_id", :null => false
     t.integer  "order_id",          :null => false
-    t.datetime "cancelled_at"
+    t.datetime "canceled_at"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
