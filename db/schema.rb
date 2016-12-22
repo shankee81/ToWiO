3aeadea72abac6faf43a8609302a8c7409c4db42
Rob Harrington <oeoeaio@gmail.com>
2016-12-22 15:58:50 +1100

    WIP: Adding flags for standing order placement and confirmation to proxy_orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20161215032136) do
+ActiveRecord::Schema.define(:version => 20161222022340) do
 
   create_table "account_invoices", :force => true do |t|
     t.integer  "user_id",    :null => false
@@ -356,6 +356,8 @@ ActiveRecord::Schema.define(:version => 20161215032136) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
     t.integer  "order_cycle_id",    :null => false
+    t.datetime "placed_at"
+    t.datetime "confirmed_at"
   end
 
   add_index "proxy_orders", ["order_cycle_id"], :name => "index_proxy_orders_on_order_cycle_id"
