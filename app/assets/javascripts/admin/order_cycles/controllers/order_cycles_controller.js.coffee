oeoeaio/openfoodnetwork

Enterprise User can create and update schedules via OC index

app/assets/javascripts/admin/order_cycles/controllers/order_cycles_controller.js.coffee
@@ -1,4 +1,4 @@
-angular.module("admin.orderCycles").controller "OrderCyclesCtrl", ($scope, $q, Columns, StatusMessage, RequestMonitor, OrderCycles, Enterprises, Schedules) ->
+angular.module("admin.orderCycles").controller "OrderCyclesCtrl", ($scope, $q, Columns, StatusMessage, RequestMonitor, OrderCycles, Enterprises, Schedules, Dereferencer) ->
   $scope.RequestMonitor = RequestMonitor
   $scope.columns = Columns.columns
   $scope.saveAll = -> OrderCycles.saveChanges($scope.order_cycles_form)
@@ -7,10 +7,13 @@ angular.module("admin.orderCycles").controller "OrderCyclesCtrl", ($scope, $q, C
 
   compileData = ->
     for schedule in $scope.schedules
-      Schedules.linkToOrderCycles(schedule)
+      Dereferencer.dereference(schedule.order_cycles, OrderCycles.orderCyclesByID)
     for orderCycle in $scope.orderCycles
-      OrderCycles.linkToEnterprises(orderCycle)
-      OrderCycles.linkToSchedules(orderCycle)
+      coordinator = Enterprises.enterprisesByID[orderCycle.coordinator.id]
+      orderCycle.coordinator = coordinator if coordinator?
+      Dereferencer.dereference(orderCycle.producers, Enterprises.enterprisesByID)
+      Dereferencer.dereference(orderCycle.shops, Enterprises.enterprisesByID)
+      Dereferencer.dereference(orderCycle.schedules, Schedules.byID)
       orderCycle.involvedEnterpriseIDs = [orderCycle.coordinator.id]
       orderCycle.producerNames = orderCycle.producers.map((producer) -> orderCycle.involvedEnterpriseIDs.push(producer.id); producer.name).join(", ")
       orderCycle.shopNames = orderCycle.shops.map((shop) -> orderCycle.involvedEnterpriseIDs.push(shop.id); shop.name).join(", ")
@@ -29,8 +32,8 @@ angular.module("admin.orderCycles").controller "OrderCyclesCtrl", ($scope, $q, C
     existingIDs = Object.keys(OrderCycles.orderCyclesByID)
     RequestMonitor.load (orderCycles = OrderCycles.index(ams_prefix: "index", "q[orders_close_at_gt]": "#{daysFromToday($scope.ordersCloseAtLimit)}", "q[id_not_in][]": existingIDs)).$promise
     orderCycles.$promise.then ->
-      compileData()
       $scope.orderCycles.push(orderCycle) for orderCycle in orderCycles
+      compileData()
 
 daysFromToday = (days) ->
   now = new Date

spec/javascripts/unit/admin/order_cycles/controllers/order_cycles_controller_spec.js.coffee
@@ -1,6 +1,6 @@
 describe "OrderCyclesCtrl", ->
-  ctrl = scope = httpBackend = Enterprises = OrderCycles = null
-  coordinator = producer = shop = orderCycle = null
+  ctrl = scope = httpBackend = Enterprises = OrderCycles = Schedules = null
+  coordinator = producer = shop = orderCycle = schedule = null
 
   beforeEach ->
     module "admin.orderCycles"
@@ -13,48 +13,62 @@ describe "OrderCyclesCtrl", ->
         compare: (actual, expected) ->
           { pass: angular.equals(actual, expected) }
 
-  beforeEach inject(($controller, $rootScope, $httpBackend, _OrderCycles_, _Enterprises_) ->
+  beforeEach inject(($controller, $rootScope, $httpBackend, _OrderCycles_, _Enterprises_, _Schedules_) ->
     scope = $rootScope.$new()
     ctrl = $controller
     httpBackend = $httpBackend
     Enterprises = _Enterprises_
     OrderCycles = _OrderCycles_
+    Schedules = _Schedules_
     spyOn(window, "daysFromToday").and.returnValue "SomeDate"
 
     coordinator = { id: 3, name: "Coordinator" }
     producer = { id: 1, name: "Producer" }
     shop = { id: 5, name: "Shop" }
-    orderCycle = { id: 4, name: "OC1", coordinator: {id: 3}, shops: [{id: 3},{id: 5}], producers: [{id: 1}] }
+    schedule = { id: 7, name: 'Weekly', order_cycles: [{id: 4}]}
+    orderCycle = { id: 4, schedules: [{id: 7}], name: "OC1", coordinator: {id: 3}, shops: [{id: 3},{id: 5}], producers: [{id: 1}] }
 
     httpBackend.expectGET("/admin/enterprises/visible.json?ams_prefix=basic").respond [coordinator, producer, shop]
+    httpBackend.expectGET("/admin/schedules.json").respond [schedule]
     httpBackend.expectGET("/admin/order_cycles.json?ams_prefix=index&q%5Borders_close_at_gt%5D=SomeDate").respond [orderCycle]
 
-    ctrl "OrderCyclesCtrl", {$scope: scope, Enterprises: Enterprises, OrderCycles: OrderCycles}
+    ctrl "OrderCyclesCtrl", {$scope: scope, Enterprises: Enterprises, OrderCycles: OrderCycles, Schedules: Schedules}
   )
 
   describe "before data is returned", ->
     it "the RequestMonitor will have a state of loading", ->
       expect(scope.RequestMonitor.loading).toBe true
 
+    it "has not received/stored any data yet", ->
+      expect(Enterprises.enterprisesByID["5"]).toBeUndefined()
+      expect(OrderCycles.orderCyclesByID["4"]).toBeUndefined()
+      expect(Schedules.byID["7"]).toBeUndefined()
+
   describe "after data is returned", ->
     beforeEach ->
       httpBackend.flush()
 
     describe "initialisation", ->
-      it "gets suppliers, adds a blank option as the first in the list", ->
-        expect(scope.enterprises).toDeepEqual [ { id : '0', name : 'All' }, coordinator, producer, shop ]
+      it "gets enterprises", ->
+        expect(scope.enterprises).toDeepEqual [ coordinator, producer, shop ]
 
-      it "stores enterprises in an list that is accessible by id", ->
-        expect(Enterprises.enterprisesByID["5"]).toDeepEqual shop
+      it "stores enterprises, order cycle and schedules in a list that is accessible by id", ->
+        expect(Enterprises.enterprisesByID["5"]).toBeDefined()
+        expect(OrderCycles.orderCyclesByID["4"]).toBeDefined()
+        expect(Schedules.byID["7"]).toBeDefined()
 
-      it "gets order cycles, with dereferenced coordinator, shops and producers", ->
+      it "gets order cycles, with dereferenced coordinator, shops and producers, schedules", ->
         oc = OrderCycles.orderCyclesByID["4"]
+        s = Schedules.byID["7"]
         expect(scope.orderCycles).toDeepEqual [oc]
         expect(oc.coordinator).toDeepEqual coordinator
         expect(oc.shops).toDeepEqual [coordinator,shop]
         expect(oc.producers).toDeepEqual [producer]
+        expect(oc.schedules).toEqual [s]
+        expect(s.order_cycles).toEqual [oc]
         expect(oc.shopNames).toEqual "Coordinator, Shop"
         expect(oc.producerNames).toEqual "Producer"
+
 
       it "the RequestMonitor will not longer have a state of loading", ->
         expect(scope.RequestMonitor.loading).toBe false

shankee81/ToWiO

app/assets/javascripts/admin/order_cycles/controllers/order_cycles_controller.js.coffee
@@ -1,4 +1,4 @@
-angular.module("admin.orderCycles").controller "OrderCyclesCtrl", ($scope, $q, Columns, StatusMessage, RequestMonitor, OrderCycles, Enterprises, Schedules) ->
+angular.module("admin.orderCycles").controller "OrderCyclesCtrl", ($scope, $q, Columns, StatusMessage, RequestMonitor, OrderCycles, Enterprises, Schedules, Dereferencer) ->
   $scope.RequestMonitor = RequestMonitor
   $scope.columns = Columns.columns
   $scope.saveAll = -> OrderCycles.saveChanges($scope.order_cycles_form)
@@ -7,10 +7,13 @@ angular.module("admin.orderCycles").controller "OrderCyclesCtrl", ($scope, $q, C
 
   compileData = ->
     for schedule in $scope.schedules
-      Schedules.linkToOrderCycles(schedule)
+      Dereferencer.dereference(schedule.order_cycles, OrderCycles.orderCyclesByID)
     for orderCycle in $scope.orderCycles
-      OrderCycles.linkToEnterprises(orderCycle)
-      OrderCycles.linkToSchedules(orderCycle)
+      coordinator = Enterprises.byID[orderCycle.coordinator.id]
+      orderCycle.coordinator = coordinator if coordinator?
+      Dereferencer.dereference(orderCycle.producers, Enterprises.byID)
+      Dereferencer.dereference(orderCycle.shops, Enterprises.byID)
+      Dereferencer.dereference(orderCycle.schedules, Schedules.byID)
       orderCycle.involvedEnterpriseIDs = [orderCycle.coordinator.id]
       orderCycle.producerNames = orderCycle.producers.map((producer) -> orderCycle.involvedEnterpriseIDs.push(producer.id); producer.name).join(", ")
       orderCycle.shopNames = orderCycle.shops.map((shop) -> orderCycle.involvedEnterpriseIDs.push(shop.id); shop.name).join(", ")
@@ -29,8 +32,8 @@ angular.module("admin.orderCycles").controller "OrderCyclesCtrl", ($scope, $q, C
     existingIDs = Object.keys(OrderCycles.orderCyclesByID)
     RequestMonitor.load (orderCycles = OrderCycles.index(ams_prefix: "index", "q[orders_close_at_gt]": "#{daysFromToday($scope.ordersCloseAtLimit)}", "q[id_not_in][]": existingIDs)).$promise
     orderCycles.$promise.then ->
-      compileData()
       $scope.orderCycles.push(orderCycle) for orderCycle in orderCycles
+      compileData()
 
 daysFromToday = (days) ->
   now = new Date

spec/javascripts/unit/admin/order_cycles/controllers/order_cycles_controller_spec.js.coffee
@@ -1,6 +1,6 @@
 describe "OrderCyclesCtrl", ->
-  ctrl = scope = httpBackend = Enterprises = OrderCycles = null
-  coordinator = producer = shop = orderCycle = null
+  ctrl = scope = httpBackend = Enterprises = OrderCycles = Schedules = null
+  coordinator = producer = shop = orderCycle = schedule = null
 
   beforeEach ->
     module "admin.orderCycles"
@@ -13,48 +13,62 @@ describe "OrderCyclesCtrl", ->
         compare: (actual, expected) ->
           { pass: angular.equals(actual, expected) }
 
-  beforeEach inject(($controller, $rootScope, $httpBackend, _OrderCycles_, _Enterprises_) ->
+  beforeEach inject(($controller, $rootScope, $httpBackend, _OrderCycles_, _Enterprises_, _Schedules_) ->
     scope = $rootScope.$new()
     ctrl = $controller
     httpBackend = $httpBackend
     Enterprises = _Enterprises_
     OrderCycles = _OrderCycles_
+    Schedules = _Schedules_
     spyOn(window, "daysFromToday").and.returnValue "SomeDate"
 
     coordinator = { id: 3, name: "Coordinator" }
     producer = { id: 1, name: "Producer" }
     shop = { id: 5, name: "Shop" }
-    orderCycle = { id: 4, name: "OC1", coordinator: {id: 3}, shops: [{id: 3},{id: 5}], producers: [{id: 1}] }
+    schedule = { id: 7, name: 'Weekly', order_cycles: [{id: 4}]}
+    orderCycle = { id: 4, schedules: [{id: 7}], name: "OC1", coordinator: {id: 3}, shops: [{id: 3},{id: 5}], producers: [{id: 1}] }
 
     httpBackend.expectGET("/admin/enterprises/visible.json?ams_prefix=basic").respond [coordinator, producer, shop]
+    httpBackend.expectGET("/admin/schedules.json").respond [schedule]
     httpBackend.expectGET("/admin/order_cycles.json?ams_prefix=index&q%5Borders_close_at_gt%5D=SomeDate").respond [orderCycle]
 
-    ctrl "OrderCyclesCtrl", {$scope: scope, Enterprises: Enterprises, OrderCycles: OrderCycles}
+    ctrl "OrderCyclesCtrl", {$scope: scope, Enterprises: Enterprises, OrderCycles: OrderCycles, Schedules: Schedules}
   )
 
   describe "before data is returned", ->
     it "the RequestMonitor will have a state of loading", ->
       expect(scope.RequestMonitor.loading).toBe true
 
+    it "has not received/stored any data yet", ->
+      expect(Enterprises.byID["5"]).toBeUndefined()
+      expect(OrderCycles.orderCyclesByID["4"]).toBeUndefined()
+      expect(Schedules.byID["7"]).toBeUndefined()
+
   describe "after data is returned", ->
     beforeEach ->
       httpBackend.flush()
 
     describe "initialisation", ->
-      it "gets suppliers, adds a blank option as the first in the list", ->
-        expect(scope.enterprises).toDeepEqual [ { id : '0', name : 'All' }, coordinator, producer, shop ]
+      it "gets enterprises", ->
+        expect(scope.enterprises).toDeepEqual [ coordinator, producer, shop ]
 
-      it "stores enterprises in an list that is accessible by id", ->
-        expect(Enterprises.enterprisesByID["5"]).toDeepEqual shop
+      it "stores enterprises, order cycle and schedules in a list that is accessible by id", ->
+        expect(Enterprises.byID["5"]).toBeDefined()
+        expect(OrderCycles.orderCyclesByID["4"]).toBeDefined()
+        expect(Schedules.byID["7"]).toBeDefined()
 
-      it "gets order cycles, with dereferenced coordinator, shops and producers", ->
+      it "gets order cycles, with dereferenced coordinator, shops and producers, schedules", ->
         oc = OrderCycles.orderCyclesByID["4"]
+        s = Schedules.byID["7"]
         expect(scope.orderCycles).toDeepEqual [oc]
         expect(oc.coordinator).toDeepEqual coordinator
         expect(oc.shops).toDeepEqual [coordinator,shop]
         expect(oc.producers).toDeepEqual [producer]
+        expect(oc.schedules).toEqual [s]
+        expect(s.order_cycles).toEqual [oc]
         expect(oc.shopNames).toEqual "Coordinator, Shop"
         expect(oc.producerNames).toEqual "Producer"
+
 
       it "the RequestMonitor will not longer have a state of loading", ->
         expect(scope.RequestMonitor.loading).toBe false