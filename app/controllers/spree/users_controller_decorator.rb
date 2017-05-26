bf11914d5b9c45da43d7530a0cfcf6bbbdde257c
Rob Harrington <oeoeaio@gmail.com>
2017-05-26 21:08:07 +1000

    Refactoring logic for displaying orders on the front-end account page

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -2,7 +2,18 @@ Spree::UsersController.class_eval do
   layout 'darkswarm'
   before_filter :set_credit_card, only: :show
 
-private
+  # Override of spree_auth_devise default
+  # Ignores invoice orders, only order where state: 'complete'
+  def show
+    @orders = @user.orders.where(state: 'complete').order('completed_at desc')
+
+    if Spree::Config.accounts_distributor_id
+      @orders = @orders.where('distributor_id != ?', Spree::Config.accounts_distributor_id)
+    end
+  end
+
+  private
+
   def set_credit_card
     @credit_card = Spree::CreditCard.new(user: @user)
   end
