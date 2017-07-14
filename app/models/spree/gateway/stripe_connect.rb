stveep/openfoodnetwork

WIP: Storing Stripe card details for later use
NOTE: No interface for actually selecting a stored card to use yet

app/models/spree/gateway/stripe_connect.rb
@@ -91,15 +91,8 @@ module Spree
       options[:currency] = gateway_options[:currency]
       options[:stripe_account] = stripe_account_id
 
-      if customer = creditcard.gateway_customer_profile_id
-        options[:customer] = customer
-      end
-      if token_or_card_id = creditcard.gateway_payment_profile_id
-        # The Stripe ActiveMerchant gateway supports passing the token directly as the creditcard parameter
-        # The Stripe ActiveMerchant gateway supports passing the customer_id and credit_card id
-        # https://github.com/Shopify/active_merchant/issues/770
-        creditcard = token_or_card_id
-      end
+      credit_card = token_from_card_profile_ids(creditcard)
+
       return money, creditcard, options
     end
 
@@ -127,6 +120,28 @@ module Spree
     def update_source!(source)
       source.cc_type = CARD_TYPE_MAPPING[source.cc_type] if CARD_TYPE_MAPPING.include?(source.cc_type)
       source
+    end
+
+    def token_from_card_profile_ids(creditcard)
+      if token_or_card_id = creditcard.gateway_payment_profile_id
+        if customer = creditcard.gateway_customer_profile_id
+          # Assume the gateway_payment_profile_id is a Stripe card_id
+          # So generate a new token, using the customer_id and card_id
+          return tokenize_instance_customer_card(customer, token_or_card_id)
+        end
+
+        # Assume the gateway_payment_profile_id is a token generated by StripeJS
+        return token_or_card_id
+      end
+    end
+
+    def tokenize_instance_customer_card(customer, card)
+      begin
+        token = Stripe::Token.create({card: card, customer: customer}, { stripe_account: stripe_account_id})
+        token.id
+      rescue Stripe::InvalidRequestError
+        # Not really sure what to do here....
+      end
     end
   end
 end

shankee81/ToWiO

app/models/spree/gateway/stripe_connect.rb
@@ -91,15 +91,8 @@ module Spree
       options[:currency] = gateway_options[:currency]
       options[:stripe_account] = stripe_account_id
 
-      if customer = creditcard.gateway_customer_profile_id
-        options[:customer] = customer
-      end
-      if token_or_card_id = creditcard.gateway_payment_profile_id
-        # The Stripe ActiveMerchant gateway supports passing the token directly as the creditcard parameter
-        # The Stripe ActiveMerchant gateway supports passing the customer_id and credit_card id
-        # https://github.com/Shopify/active_merchant/issues/770
-        creditcard = token_or_card_id
-      end
+      creditcard = token_from_card_profile_ids(creditcard)
+
       return money, creditcard, options
     end
 
@@ -127,6 +120,28 @@ module Spree
     def update_source!(source)
       source.cc_type = CARD_TYPE_MAPPING[source.cc_type] if CARD_TYPE_MAPPING.include?(source.cc_type)
       source
+    end
+
+    def token_from_card_profile_ids(creditcard)
+      if token_or_card_id = creditcard.gateway_payment_profile_id
+        if customer = creditcard.gateway_customer_profile_id
+          # Assume the gateway_payment_profile_id is a Stripe card_id
+          # So generate a new token, using the customer_id and card_id
+          return tokenize_instance_customer_card(customer, token_or_card_id)
+        end
+
+        # Assume the gateway_payment_profile_id is a token generated by StripeJS
+        return token_or_card_id
+      end
+    end
+
+    def tokenize_instance_customer_card(customer, card)
+      begin
+        token = Stripe::Token.create({card: card, customer: customer}, { stripe_account: stripe_account_id})
+        token.id
+      rescue Stripe::InvalidRequestError
+        # Not really sure what to do here....
+      end
     end
   end
 end